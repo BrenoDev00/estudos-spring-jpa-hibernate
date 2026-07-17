e459b8ffe25e57975de5ebcad540834d5955bfd5
Breno Gonçalves <breno.goncalves@datatraffic.com.br>
2026-07-17 14:31:24 -0300

    configura propriedades do banco e entidade User

--- projeto-curso/src/main/java/com/projetos/projeto_curso/entities/User.java
+++ projeto-curso/src/main/java/com/projetos/projeto_curso/entities/User.java
@@ -1,6 +1,15 @@
 package com.projetos.projeto_curso.entities;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
+@Entity
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
